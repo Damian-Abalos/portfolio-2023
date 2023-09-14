709d90a87b7b6237da3fbc5f2fcc48cfcd2879c7
Damian <damianabalos@hotmail.com>
2023-09-14 02:43:38 -0300

    fix netlify-form

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -123,57 +123,42 @@ const Contacto = () => {
         <div className="contact-wrapper">
 
 
-          <form name="contact" netlify>
-            <p>
-              <label>Name <input type="text" name="name" /></label>
-            </p>
-            <p>
-              <label>Email <input type="email" name="email" /></label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-        {/* <form id="contact-form" className="form-horizontal" name='contact' netlify>
-
+          <form className="form-horizontal" name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
             <div className="form-group">
               <div className="col-sm-12">
-                <input type="text" className="form-control" id="name" placeholder="NAME" name="name" required />
+                <input type="text" name="name" className="form-control" placeholder="NAME" required />
               </div>
             </div>
-
             <div className="form-group">
               <div className="col-sm-12">
-                <input type="email" className="form-control" id="email" placeholder="EMAIL" name="email" required />
+                <input type="email" name="email" className="form-control" placeholder="EMAIL" required />
               </div>
             </div>
-
-            <textarea className="form-control" rows="10" placeholder="MESSAGE" name="message" id='message' required></textarea>
-            <input type="hidden" name="form-name" value="Contact" />
-            <button className="btn btn-primary send-button" id="submit" type="submit" value="SEND">
+            <textarea name="message" className="form-control" rows="10" placeholder="MESSAGE" required></textarea>
+            <button type="submit" className="btn btn-primary send-button">
               <div className="alt-send-button">
                 <i className="fa fa-paper-plane"></i><span className="send-text">SEND</span>
               </div>
 
             </button>
-
-          </form> */}
-
-
-        <div className="direct-contact-container">
-
-          <ul className="contact-list">
-            <li className="list-item"><i className="fa fa-map-marker fa-2x"><span className="contact-text place">Buenos Aires</span></i></li>
-
-            <li className="list-item"><i className="fa fa-phone fa-2x"><span className="contact-text phone"><a href="tel:11-3371-0828" title="Give me a call">(011) 3371-0828</a></span></i></li>
-
-            <li className="list-item"><i className="fa fa-envelope fa-2x"><span className="contact-text gmail"><a href="mailto:#" title="Send me an email">damianabalos94@gmail.com</a></span></i></li>
-
-          </ul>
+          </form>
 
 
+          <div className="direct-contact-container">
+
+            <ul className="contact-list">
+              <li className="list-item"><i className="fa fa-map-marker fa-2x"><span className="contact-text place">Buenos Aires</span></i></li>
+
+              <li className="list-item"><i className="fa fa-phone fa-2x"><span className="contact-text phone"><a href="tel:11-3371-0828" title="Give me a call">(011) 3371-0828</a></span></i></li>
+
+              <li className="list-item"><i className="fa fa-envelope fa-2x"><span className="contact-text gmail"><a href="mailto:#" title="Send me an email">damianabalos94@gmail.com</a></span></i></li>
+
+            </ul>
+
+
+          </div>
         </div>
-    </div>
       </section >
     </div >
   )
